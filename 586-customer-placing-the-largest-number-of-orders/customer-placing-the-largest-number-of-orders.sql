8df8b67e29c3e6d84dee2f8457f656a65439a9eb
GANESH GUDALA <ganeshgudala2003@gmail.com>
2025-09-02 18:48:20 +0530

    Time: 490 ms (43.86%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,9 +1,1 @@
-SELECT
-    customer_number
-FROM
-    orders
-GROUP BY
-    customer_number
-ORDER BY
-    COUNT(order_number) DESC
-LIMIT 1;
+SELECT customer_number FROM orders GROUP BY customer_number ORDER BY COUNT(order_number) DESC LIMIT 1;
